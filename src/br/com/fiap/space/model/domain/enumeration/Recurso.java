ff4ab77efc09db766b3837ae704b5242db9d1087
Rafael <seijiarakaki12@gmail.com>
2026-06-08 01:16:41 -0300

    atualizando os enumerations

--- src/br/com/fiap/space/model/domain/enumeration/Recurso.java
+++ src/br/com/fiap/space/model/domain/enumeration/Recurso.java
@@ -1,4 +1,25 @@
 package br.com.fiap.space.model.domain.enumeration;
 
 public enum Recurso {
+    //Tipos de recursos possiveis de encontrar
+
+    FERRO("Minério de Ferro", 5.0),
+    GELO("Água em Gelo", 3.5),
+    REGOLITO("Regolito", 2.2);
+
+    private final String nomeExibicao;
+    private final double pesoUnidade;
+
+    private Recurso(String nomeExibicao, double pesoUnidade) {
+        this.nomeExibicao = nomeExibicao;
+        this.pesoUnidade = pesoUnidade;
+    }
+
+    public String getNomeExibicao() {
+        return nomeExibicao;
+    }
+
+    public double getPesoUnidade() {
+        return pesoUnidade;
+    }
 }
--- src/br/com/fiap/space/model/domain/enumeration/Terreno.java
+++ src/br/com/fiap/space/model/domain/enumeration/Terreno.java
@@ -1,10 +1,12 @@
 package br.com.fiap.space.model.domain.enumeration;
 
 public enum Terreno {
+    //Opções de terrenos em solo extraterrestre
 
     PLANICE(10.0),
-    ROCHOSO(15.0),
-    MONTANHOSO(25.0)
+    CRATERA(15.0),
+    CRATERA_PROFUNDA(15.0),
+    ROCHOSO(25.0)
     ;
 
     private final double custoEnergia;
